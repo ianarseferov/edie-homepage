7343d5eda8a395eab051f568118a63039fb06fe9
Anar Səfərov <1anarseferov@gmail.com>
2021-08-03 18:00:06 +0400

    hamburger toggle

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector("nav");
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+});
