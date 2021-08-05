10cf14a611d941d04280b37df9da0bc189657ffb
Anar Səfərov <1anarseferov@gmail.com>
2021-08-05 14:04:12 +0400

    added toggle menu for navMenu

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector("nav");
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
-//   navMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
